9eebfd407105934cead54a4b911d3cef30e40afc
eduardo talavera <talaverae4@gmail.com>
2020-09-26 15:29:55 -0500

    make StoryRel, MessageSender and Sidebar components

---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css';
+
+function Post() {
+  return (
+    <div className="post">
+      <h3>I'm a post...</h3>
+    </div>
+  )
+}
+
+export default Post;
